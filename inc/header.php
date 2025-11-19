4de6c2d21fecc7289ad6464818dbaf25bf606ba2
SPLprod <liliandanjou@gmail.com>
2025-11-19 14:19:14 +0100

    navbar responsive :D

--- inc/header.php
+++ inc/header.php
@@ -18,10 +18,15 @@ session_start();
         <nav>
             <a href="/PHP-e-commerce/">Accueil</a>
 
-            <a href="/PHP-e-commerce/register">S'inscrire</a>
-            <a href="/PHP-e-commerce/login">Se connecter</a>
-            <a href="/PHP-e-commerce/dashboard">Admin</a>
-            <a href="/PHP-e-commerce/cart">Panier</a>
+            <?php 
+                if (isset($_SESSION["isconnect"]) && $_SESSION["isconnect"] == true) {
+                    echo "<a href='/PHP-e-commerce/dashboard'>Admin</a>" . "<a href='/PHP-e-commerce/cart'>Panier</a>";
+                } else {
+                    echo "<a href='/PHP-e-commerce/register'>S'inscrire</a>" . "<a href='/PHP-e-commerce/login'>Se connecter</a>";
+                }
+            ?>
+            
+            
         </nav>
     </header>
     <main>
