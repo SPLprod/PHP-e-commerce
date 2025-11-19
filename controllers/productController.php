cfe622c3c34377c7644fef3a54dd1101845ec5a2
SPLprod <liliandanjou@gmail.com>
2025-11-19 16:59:25 +0100

    oui

--- controllers/productController.php
+++ controllers/productController.php
@@ -1,10 +1,24 @@
 <?php
+include_once __DIR__ . "/../database/db.php";
 
 
-
-function getAllProducts()
+function getAllProducts($x)
 {
   // requetes sql pour chopper tous les produits
+  $result = $x->querry("SELECT * FROM products");
+        $result = $result->fetch(PDO::FETCH_ASSOC);
+        var_dump($result);
+
+  // foreach ($result as $key => $value) {
+  //       echo 
+  //       '<div class="product">
+  //       <h2></h2>
+  //       <img src="" alt="" />
+  //       <p>Prix: €</p>
+  //       <a href="">Voir détails</a>
+  //       </div>'
+  //       ;
+  //   }
 }
 
 function getProductById($id) {}
--- views/index.php
+++ views/index.php
@@ -1,15 +1,24 @@
 <?php
 include __DIR__ . '/../inc/header.php';
+include_once __DIR__ . "/../database/db.php";
 ?>
 <h1>Bienvenue dans notre boutique</h1>
 <div class="products">
 
-    <div class="product">
-        <h2></h2>
-        <img src="" alt="" />
-        <p>Prix: €</p>
-        <a href="">Voir détails</a>
-    </div>
+    <?php 
+        $pdo = new PDO(
+        'mysql:host=localhost;dbname=site_e-commerce',
+        // driver mysql + nom du serveur de la BDD + nom de la BDD 
+        'root',   // pseudo de la BDD
+        '',   // mdp de la BDD 'root' pour MAC et '' pour Windows
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  // pour afficher les messages d'erreur SQL
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'  // définition du jeu de caractères des échanges avec la BDD
+        ]
+        );
+        getAllProducts($pdo);
+    ?>
+    
 
     <p>Aucun produit à afficher pour le moment.</p> <!-- Message si aucun produit n'est disponible -->
 
